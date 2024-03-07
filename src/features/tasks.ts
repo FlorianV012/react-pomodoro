3f9a0b9d4e668db583bbc38c8db19bfce6437895
Florian <fvilmot@gmail.com>
2024-03-07 16:43:15 +0100

    adding task recording to local storage

--- src/features/tasks.ts
+++ src/features/tasks.ts
@@ -5,14 +5,33 @@ interface IInitialState {
   tasks: ITask[];
 }
 
-const initialState: IInitialState = {
-  tasks: [
-    { id: 1, content: "Wall of <3", done: false, inProgress: false },
-    { id: 2, content: "Research talk slides", done: true, inProgress: false },
-    { id: 3, content: "Inbox 0", done: false, inProgress: false },
-    { id: 4, content: "design list of tasks", done: false, inProgress: false },
-  ],
+const getInitialStateFromLocalStorage = (): IInitialState => {
+  const savedTasks = localStorage.getItem("dashboard");
+  if (savedTasks) {
+    return { tasks: JSON.parse(savedTasks) };
+  } else {
+    return {
+      tasks: [
+        { id: 1, content: "Wall of <3", done: false, inProgress: false },
+        {
+          id: 2,
+          content: "Research talk slides",
+          done: true,
+          inProgress: false,
+        },
+        { id: 3, content: "Inbox 0", done: false, inProgress: false },
+        {
+          id: 4,
+          content: "design list of tasks",
+          done: false,
+          inProgress: false,
+        },
+      ],
+    };
+  }
 };
+
+const initialState: IInitialState = getInitialStateFromLocalStorage();
 
 const tasksSlice = createSlice({
   name: "tasks",
@@ -20,6 +39,7 @@ const tasksSlice = createSlice({
   reducers: {
     addNewTask: (state, action: PayloadAction<ITask>) => {
       state.tasks = [...state.tasks, action.payload];
+      localStorage.setItem("dashboard", JSON.stringify(state.tasks));
     },
     taskIsDone: (state, action: PayloadAction<ITask>) => {
       const taskId = action.payload.id;
@@ -29,6 +49,7 @@ const tasksSlice = createSlice({
       ) as ITask;
       if (taskToUpdate) {
         taskToUpdate.done = !taskToUpdate.done;
+        localStorage.setItem("dashboard", JSON.stringify(state.tasks));
       }
     },
     taskInProgress: (state, action: PayloadAction<ITask>) => {
@@ -41,11 +62,13 @@ const tasksSlice = createSlice({
           task.inProgress = false;
         }
       });
+      localStorage.setItem("dashboard", JSON.stringify(state.tasks));
     },
     deleteTask: (state, action: PayloadAction<ITask>) => {
       const taskId = action.payload.id;
 
       state.tasks = state.tasks.filter((task) => task.id !== taskId);
+      localStorage.setItem("dashboard", JSON.stringify(state.tasks));
     },
   },
 });
